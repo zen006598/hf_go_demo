2d0288e2cd689fb8fb0bcb439e47a76ff6d5cae1
h3098(jeff) <JungWei.Chen@housefun.com.tw>
2025-03-27 18:35:27 +0800

    add var demo

---
+++ ex1/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// var x int = 10
+// x:= 10
+func main() {
+	// var 變數名稱 資料型態 = value
+	var x int = 10
+	// var 變數名稱 資料型態
+	var y int
+	y = 20
+	// var x, y int = 10, 20
+	// var x, y = 10, 20
+	// var x, y = 10, "xxx"
+	// // var const 都可包軌
+	// var (
+	// 	x int = 10
+	// 	y     = 20
+	// )
+	// x := 10
+	fmt.Println(x)
+	fmt.Println(y)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zen006598/goDemo
+
+go 1.24.1
